0d1131c3fa6345d00a48b0f7a3729eb1e94dc600
Renz Jake Castro <85826085+xBlueRose@users.noreply.github.com>
2022-03-16 14:03:15 +0800

    Initial commit

---
+++ src/main/java/com/Hackathon/Toro/RestfulAPI/RestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.Hackathon.Toro.RestfulAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hackathon/Toro/RestfulAPI/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hackathon.Toro.RestfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
